2695f9357848b3128efcbb0c3de7b9c13e837986
avazbekowna <almamatova.erkaiym03@gmail.com>
2023-01-09 12:46:23 +0600

    hero add

--- src/componets/Hero/Hero.js
+++ src/componets/Hero/Hero.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div>
-            Hero
-        </div>
+        <section className="hero">
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, iure.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae explicabo laborum sequi voluptates voluptatum! Adipisci alias animi aperiam aut consequuntur cupiditate debitis dolor dolore ea eius est fugit inventore ipsam itaque iusto laborum libero maiores modi nemo, nostrum nulla placeat quas quia quisquam, quo repellendus sint temporibus veritatis vero voluptates? Ducimus est hic reprehenderit ut. Ipsam, labore rerum. Commodi deserunt excepturi in labore, libero officia, perferendis quasi quis quo ratione rem sapiente veritatis? Aliquam animi aperiam autem earum esse illo inventore iure, laudantium magni obcaecati odio pariatur, perferendis possimus provident quasi quibusdam quisquam quo repellendus, reprehenderit repudiandae tenetur totam voluptatem.</p>
+        </section>
     );
 };
 
