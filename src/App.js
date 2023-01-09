2a0b735c46e35951e1a3fdc318189bb366445e87
nurbekovv05 <baevnurbol@gmail.com>
2023-01-09 12:40:45 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
 import './App.css';
+import Header from "./componets/Header/Header";
+import Hero from "./componets/Hero/Hero";
+import Footer from "./componets/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Hero/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
